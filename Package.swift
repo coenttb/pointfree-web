6fdb3612b3db916e87c04f27628448f56c9f4677
Coen ten Thije Boonkkamp <coen@tenthijeboonkkamp.nl>
2024-12-21 20:59:13 +0100

    wip

--- Package.swift
+++ Package.swift
@@ -34,7 +34,6 @@ extension Target.Dependency {
     static var crypto: Self { .product(name: "Crypto", package: "swift-crypto") }
     static var cryptor: Self { .product(name: "Cryptor", package: "BlueCryptor") }
     static var dependencies: Self { .product(name: "Dependencies", package: "swift-dependencies") }
-    static var dependenciesMacros: Self { .product(name: "DependenciesMacros", package: "swift-dependencies") }
     static var either: Self { .product(name: "Either", package: "swift-prelude") }
     static var logging: Self { .product(name: "Logging", package: "swift-log") }
     static var nio: Self { .product(name: "NIO", package: "swift-nio") }
@@ -42,8 +41,6 @@ extension Target.Dependency {
     static var nioEmbedded: Self { .product(name: "NIOEmbedded", package: "swift-nio") }
     static var nioHTTP1: Self { .product(name: "NIOHTTP1", package: "swift-nio") }
     static var nioHTTPCompression: Self { .product(name: "NIOHTTPCompression", package: "swift-nio-extras") }
-    static var macroCodableKit: Self { .product(name: "MacroCodableKit", package: "macro-codable-kit") }
-    static var memberwiseInit: Self { .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro") }
     static var postgresKit: Self { .product(name: "PostgresKit", package: "postgres-kit") }
     static var optics: Self { .product(name: "Optics", package: "swift-prelude") }
     static var swiftHtml: Self { .product(name: "HTML", package: "swift-html") }
@@ -60,14 +57,12 @@ extension [Package.Dependency] {
             .package(url: "https://github.com/apple/swift-nio-extras.git", from: "1.0.0"),
             .package(url: "https://github.com/apple/swift-log", from: "1.5.0"),
             .package(url: "https://github.com/coenttb/swift-html", branch: "main"),
-            .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro", from: "0.3.0"),
             .package(url: "https://github.com/IBM-Swift/BlueCryptor.git", from: "1.0.0"),
             .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.1.5"),
             .package(url: "https://github.com/pointfreeco/swift-tagged.git", from: "0.10.0"),
             .package(url: "https://github.com/pointfreeco/swift-url-routing", from: "0.6.0"),
             .package(url: "https://github.com/swift-server/async-http-client", from: "1.19.0"),
             .package(url: "https://github.com/vapor/postgres-kit", from: "2.12.0"),
-            .package(url: "https://github.com/mikhailmaslo/macro-codable-kit.git", from: "0.3.0"),
             .package(url: "https://github.com/pointfreeco/swift-prelude.git", branch: "main"),
         ]
     }
